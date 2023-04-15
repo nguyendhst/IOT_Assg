73f1a1e80778a4c2793e68e8bd5568d8e9fe4d62
NgTrNghia1211 <106511805+NgTrNghia1211@users.noreply.github.com>
2023-04-15 22:49:35 +0700

    upload PortManagement

--- OOP/AdafruitConnect.py
+++ OOP/AdafruitConnect.py
@@ -9,15 +9,20 @@ class AdafruitConnect:
             self.feed_ids = feed_ids
             self.client = MQTTClient(self.username, self.key)
 
+            self.client.on_message = self.message
+
         def connect(self):
             self.client.connect()
 
-        def subcribe(self):
+        def subscribe(self):
             for feed in self.feed_ids:
                 self.client.subscribe(feed)
 
         def publish(self, feed, data):
             self.client.publish(feed, data)
+
+        def message(client, feed, payload):
+            print("Received: " + payload + ", feed_id: " + feed)
 
         def run(self):
             self.client.loop_background()
--- OOP/main.py
+++ OOP/main.py
@@ -1,15 +1,20 @@
 import time
+import random
 from AdafruitConnect import AdafruitConnect
+from PortManagement import PortManagement
 
 
 AIO_FEED_IDS = ["button1", "button2"]
 AIO_USERNAME = "xMysT"
-AIO_KEY = "aio_OcUq99IvIe55uNA8OM0SCbUaI5vP"
+AIO_KEY = "aio_ojbG10jK1n7p7V1gSElP7xuttiUV"
 
 gateway = AdafruitConnect(AIO_USERNAME, AIO_KEY, AIO_FEED_IDS)
 gateway.connect()
-gateway.subcribe()
+gateway.subscribe()
+
+port = PortManagement()
 
 while True:
+    port.readSerial(gateway)
     gateway.run()
     time.sleep(1)
